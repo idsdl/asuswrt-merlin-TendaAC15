7ac30f9971c2c4c002954dbe29eadad18f919454
id <id@Centos9.localdomain>
2020-04-23 16:22:54 +0300

    clear any bak files

--- release/src/router/getdns/build/CMakeFiles/getdns.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libgetdns.pdb"
-  "libgetdns.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/getdns.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- release/src/router/openssl-1.1/crypto/rc5/rc5_skey.d
+++
@@ -1,3 +0,0 @@
-crypto/rc5/rc5_skey.o: crypto/rc5/rc5_skey.c include/openssl/rc5.h \
- include/openssl/opensslconf.h include/openssl/opensslv.h \
- crypto/rc5/rc5_local.h
